ff402091f6aa8ceb9f8e6b9827aa1c2668f57273
Manuel Serquet <manuelserquet@hotmail.com>
2017-01-15 18:08:05 +0100

    NEON_WALLS

--- src/main/java/ch/fhnw/comgr/tron/models/Wall.java
+++ src/main/java/ch/fhnw/comgr/tron/models/Wall.java
@@ -1,5 +1,6 @@
 package ch.fhnw.comgr.tron.models;
 
+import ch.fhnw.comgr.tron.render.WallMaterial;
 import ch.fhnw.ether.controller.IController;
 import ch.fhnw.ether.image.IGPUImage;
 import ch.fhnw.ether.scene.IScene;
@@ -35,7 +36,7 @@ public class Wall {
     private final Player playerA;
     private final Player playerB;
 
-    private IMaterial sideMaterial;
+    private WallMaterial sideMaterial;
     private IMaterial topMaterial;
     private float[] sideColors;
     private float[] topColors;
@@ -89,6 +90,9 @@ public class Wall {
             } else {
                 stopWallBuilding();
             }
+
+            //sideMaterial.setWhiteGain((float) time % 1);
+            sideMaterial.setWhiteGain((float) Math.sin(time) + 1f);
         });
     }
 
@@ -104,12 +108,14 @@ public class Wall {
     private void createMaterial() {
         IGPUImage t = null;
         try {
-            t = IGPUImage.read(Wall.class.getResource("/assets/textures/Microscheme.jpg"));
+            t = IGPUImage.read(Wall.class.getResource("/assets/textures/Microscheme2.jpg"));
+            t = IGPUImage.read(Wall.class.getResource("/textures/tron_wall.jpg"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        sideMaterial = new ColorMapMaterial(team.getTeamColor(), t, true);
+        //sideMaterial = new ColorMapMaterial(team.getTeamColor(), t, true);
+        sideMaterial = new WallMaterial(team.getTeamColor(), t);
         topMaterial  = new ColorMaterial(team.getTeamColor());
     }
 
